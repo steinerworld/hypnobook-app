1ae8580e8277c458f7de2348117a749444e41838
michael <i009Grande>
2022-09-19 00:33:22 +0200

    kleine Schritte

--- src/main/java/net/steinerworld/hypnobook/ui/views/buchung/BuchungView.java
+++ src/main/java/net/steinerworld/hypnobook/ui/views/buchung/BuchungView.java
@@ -11,13 +11,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Sort;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.BinderValidationStatus;
 import com.vaadin.flow.router.PageTitle;
@@ -26,13 +31,13 @@ import com.vaadin.flow.router.Route;
 import lombok.RequiredArgsConstructor;
 import net.steinerworld.hypnobook.domain.Buchung;
 import net.steinerworld.hypnobook.domain.BuchungType;
+import net.steinerworld.hypnobook.domain.Kategorie;
 import net.steinerworld.hypnobook.domain.Steuerperiode;
 import net.steinerworld.hypnobook.domain.SteuerperiodeState;
 import net.steinerworld.hypnobook.exceptions.MaloneyException;
 import net.steinerworld.hypnobook.repository.BuchungRepository;
 import net.steinerworld.hypnobook.repository.KategorieRepository;
 import net.steinerworld.hypnobook.repository.SteuerperiodeRepository;
-import net.steinerworld.hypnobook.ui.components.buchung.BuchungEditForm;
 import net.steinerworld.hypnobook.ui.views.MainLayout;
 
 @PermitAll
@@ -47,6 +52,11 @@ public class BuchungView extends VerticalLayout {
    private final BeanValidationBinder<Buchung> buchungBinder = new BeanValidationBinder<>(Buchung.class);
    private final Grid<Buchung> grid = new Grid<>();
 
+   private final VerticalLayout tabContent = new VerticalLayout();
+   private final Tab ausgabeTab = new Tab("Ausgabe");
+   private final Tab einnahmeTab = new Tab("Einnahme");
+
+
    private Dialog ausgabeDialog;
    private Dialog einnahmeDialog;
 
@@ -56,24 +66,26 @@ public class BuchungView extends VerticalLayout {
 
       add(createBuchhaltungGrid());
 
-      BuchungEditForm ausgabeForm = BuchungEditForm.builder().kategorieList(kategorieRepository::findAll)
-            .periodeList(periodeRepository::findAll)
-            .binder(buchungBinder)
-            .type(BuchungType.AUSGABE)
-            .build().get();
-      ausgabeDialog = createBuchungDialog("Ausgabe", ausgabeForm);
-      Button ausgabeButton = buildOpenDialogButton(BuchungType.AUSGABE, "Neue Ausgabe erfassen", ausgabeDialog);
+      add(createBuchungTab(), tabContent);
 
-      BuchungEditForm einnahmeForm = BuchungEditForm.builder().kategorieList(kategorieRepository::findAll)
-            .periodeList(periodeRepository::findAll)
-            .binder(buchungBinder)
-            .type(BuchungType.EINNAHME)
-            .build().get();
-      einnahmeDialog = createBuchungDialog("Einnahme", einnahmeForm);
-      Button einnahmeButton = buildOpenDialogButton(BuchungType.EINNAHME, "Neue Einnahme erfassen", einnahmeDialog);
-
-      HorizontalLayout dialogButtons = new HorizontalLayout(ausgabeButton, einnahmeButton);
-      add(ausgabeDialog, einnahmeDialog, dialogButtons);
+      //      BuchungEditForm ausgabeForm = BuchungEditForm.builder().kategorieList(kategorieRepository::findAll)
+      //            .periodeList(periodeRepository::findAll)
+      //            .binder(buchungBinder)
+      //            .type(BuchungType.AUSGABE)
+      //            .build().get();
+      //      ausgabeDialog = createBuchungDialog("Ausgabe", ausgabeForm);
+      //      Button ausgabeButton = buildOpenDialogButton(BuchungType.AUSGABE, "Neue Ausgabe erfassen", ausgabeDialog);
+      //
+      //      BuchungEditForm einnahmeForm = BuchungEditForm.builder().kategorieList(kategorieRepository::findAll)
+      //            .periodeList(periodeRepository::findAll)
+      //            .binder(buchungBinder)
+      //            .type(BuchungType.EINNAHME)
+      //            .build().get();
+      //      einnahmeDialog = createBuchungDialog("Einnahme", einnahmeForm);
+      //      Button einnahmeButton = buildOpenDialogButton(BuchungType.EINNAHME, "Neue Einnahme erfassen", einnahmeDialog);
+      //
+      //      HorizontalLayout dialogButtons = new HorizontalLayout(ausgabeButton, einnahmeButton);
+      //      add(ausgabeDialog, einnahmeDialog, dialogButtons);
    }
 
    private Grid<Buchung> createBuchhaltungGrid() {
@@ -98,39 +110,146 @@ public class BuchungView extends VerticalLayout {
       return grid;
    }
 
-   private Button buildOpenDialogButton(BuchungType type, String text, Dialog dialog) {
-      return new Button(text, e -> {
-         buchungBinder.setBean(newBuchhaltung(type));
-         dialog.open();
-      });
+   private Tabs createBuchungTab() {
+      Tabs tabs = new Tabs(ausgabeTab, einnahmeTab);
+      tabs.addSelectedChangeListener(event ->
+            setTabContent(event.getSelectedTab())
+      );
+      return tabs;
    }
 
-   private void loadGridData() {
-      grid.setItems(buchungRepository.findAll(Sort.by(Sort.Direction.DESC, "buchungsdatum", "id")));
+   private void setTabContent(Tab tab) {
+      tabContent.removeAll();
+
+      if (tab.equals(ausgabeTab)) {
+         tabContent.add(createAusgabeForm(), createSaveButton());
+      } else if (tab.equals(einnahmeTab)) {
+         tabContent.add(createEinnahmeForm());
+      }
    }
 
-   private Dialog createBuchungDialog(String title, FormLayout form) {
-      Dialog dialog = new Dialog();
-      dialog.setModal(true);
-      dialog.setHeaderTitle(title);
-      dialog.add(form);
-      Button saveButton = new Button("Buchen", e -> {
+   private FormLayout createAusgabeForm() {
+      FormLayout layout = createFormlayout();
+
+      TextField belegNrTextField = new TextField("Beleg-Nr.");
+      buchungBinder.forField(belegNrTextField).bind(Buchung::getBelegNr, Buchung::setBelegNr);
+
+      NumberField betragNumberField = new NumberField("Betrag in CHF");
+      Div chfSuffix = new Div();
+      chfSuffix.setText("CHF");
+      betragNumberField.setSuffixComponent(chfSuffix);
+      buchungBinder.forField(betragNumberField).bind(Buchung::getAusgabe, Buchung::setAusgabe);
+
+      DatePicker buchungsdatumDatePicker = new DatePicker("Datum");
+      buchungBinder.forField(buchungsdatumDatePicker)
+            .withValidator(this::inValidPeriode, "Buchung in geschlossener oder noch nicht erfasster Steuerperiode")
+            .bind(Buchung::getBuchungsdatum, Buchung::setBuchungsdatum);
+
+      Select<Kategorie> kategorieSelect = new Select<>();
+      kategorieSelect.setLabel("Kategorie");
+      kategorieSelect.setItemLabelGenerator(Kategorie::getName);
+      kategorieSelect.setItems(kategorieRepository.findAll());
+      buchungBinder.forField(kategorieSelect).bind(Buchung::getKategorie, Buchung::setKategorie);
+
+      TextField textTextField = new TextField("Text");
+      layout.setColspan(textTextField, 2);
+      buchungBinder.forField(textTextField).bind(Buchung::getText, Buchung::setText);
+
+      layout.add(belegNrTextField, betragNumberField, buchungsdatumDatePicker, kategorieSelect, textTextField);
+      return layout;
+   }
+
+   private FormLayout createEinnahmeForm() {
+      FormLayout layout = createFormlayout();
+
+      TextField belegNrTextField = new TextField("Beleg-Nr.");
+      buchungBinder.forField(belegNrTextField).bind(Buchung::getBelegNr, Buchung::setBelegNr);
+
+      NumberField betragNumerField = new NumberField("Betrag in CHF");
+      Div chfSuffix = new Div();
+      chfSuffix.setText("CHF");
+      betragNumerField.setSuffixComponent(chfSuffix);
+      buchungBinder.forField(betragNumerField).bind(Buchung::getEinnahme, Buchung::setEinnahme);
+
+      DatePicker buchungsdatumDatePicker = new DatePicker("Datum");
+      buchungBinder.forField(buchungsdatumDatePicker)
+            .withValidator(this::inValidPeriode, "Buchung in geschlossener oder noch nicht erfasster Steuerperiode")
+            .bind(Buchung::getBuchungsdatum, Buchung::setBuchungsdatum);
+
+      DatePicker zahlungseingangDatePicker = new DatePicker("Zahlungseingang");
+      buchungBinder.forField(zahlungseingangDatePicker).bind(Buchung::getEingangsdatum, Buchung::setEingangsdatum);
+
+      TextField textTextField = new TextField("Text");
+      layout.setColspan(textTextField, 2);
+      buchungBinder.forField(textTextField).bind(Buchung::getText, Buchung::setText);
+
+      layout.add(belegNrTextField, betragNumerField, buchungsdatumDatePicker, zahlungseingangDatePicker, textTextField);
+      return layout;
+   }
+
+   private FormLayout createFormlayout() {
+      FormLayout layout = new FormLayout();
+      layout.setResponsiveSteps(
+            new FormLayout.ResponsiveStep("0", 1),
+            new FormLayout.ResponsiveStep("500px", 3)
+      );
+      return layout;
+   }
+
+   private boolean inValidPeriode(LocalDate date) {
+      return periodeRepository.findAll().stream()
+            .filter(periode -> periode.getStatus() != SteuerperiodeState.GESCHLOSSEN)
+            .filter(periode -> date.isAfter(periode.getVon()))
+            .anyMatch(periode -> date.isBefore(periode.getBis()));
+   }
+
+   private Button createSaveButton() {
+      return new Button("Buchen", e -> {
          BinderValidationStatus<Buchung> validate = buchungBinder.validate();
          if (validate.isOk()) {
             Buchung bean = buchungBinder.getBean();
             bean.setSteuerperiode(getPeriodeFromNew(bean));
             saveBuchungAndRefresh(bean);
-            dialog.close();
          } else {
             Notification.show("Keine valide Buchung");
          }
       });
-      saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-      Button cancelButton = new Button("Cancel", e -> dialog.close());
-      dialog.getFooter().add(cancelButton);
-      dialog.getFooter().add(saveButton);
-      return dialog;
    }
+
+
+   //   private Button buildOpenDialogButton(BuchungType type, String text, Dialog dialog) {
+   //      return new Button(text, e -> {
+   //         buchungBinder.setBean(newBuchhaltung(type));
+   //         dialog.open();
+   //      });
+   //   }
+
+   private void loadGridData() {
+      grid.setItems(buchungRepository.findAll(Sort.by(Sort.Direction.DESC, "buchungsdatum", "id")));
+   }
+
+   //   private Dialog createBuchungDialog(String title, FormLayout form) {
+   //      Dialog dialog = new Dialog();
+   //      dialog.setModal(true);
+   //      dialog.setHeaderTitle(title);
+   //      dialog.add(form);
+   //      Button saveButton = new Button("Buchen", e -> {
+   //         BinderValidationStatus<Buchung> validate = buchungBinder.validate();
+   //         if (validate.isOk()) {
+   //            Buchung bean = buchungBinder.getBean();
+   //            bean.setSteuerperiode(getPeriodeFromNew(bean));
+   //            saveBuchungAndRefresh(bean);
+   //            dialog.close();
+   //         } else {
+   //            Notification.show("Keine valide Buchung");
+   //         }
+   //      });
+   //      saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+   //      Button cancelButton = new Button("Cancel", e -> dialog.close());
+   //      dialog.getFooter().add(cancelButton);
+   //      dialog.getFooter().add(saveButton);
+   //      return dialog;
+   //   }
 
    private Steuerperiode getPeriodeFromNew(Buchung item) {
       return periodeRepository.findAll().stream()
