62dce9fd332740c38859460f163adeb022646c96
Michael Steiner <michael@steinerworld.net>
2022-10-08 00:50:09 +0200

    Dashboard umbau zu charts

--- src/main/java/net/steinerworld/hypnobook/ui/views/dashboard/DashboardView.java
+++ src/main/java/net/steinerworld/hypnobook/ui/views/dashboard/DashboardView.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -14,14 +15,28 @@ import net.steinerworld.hypnobook.ui.views.MainLayout;
 @PermitAll
 @PageTitle("Hypno Book - Übersicht")
 @Route(value = "dashboard", layout = MainLayout.class)
-public class DashboardView extends Div {
+public class DashboardView extends VerticalLayout {
    private static final Logger LOGGER = LoggerFactory.getLogger(DashboardView.class);
 
    public DashboardView() {
       LOGGER.info(">>>> CHART");
       setSizeFull();
 
+      addBarChart();
+      addPieChart();
+   }
+
+   private void addBarChart() {
       VerticalBarChartExample example = new VerticalBarChartExample();
-      add(example.build());
+      Div div = new Div(example.build());
+      div.setWidth("50%");
+      add(div);
+   }
+
+   private void addPieChart() {
+      PieChartExample example = new PieChartExample();
+      Div div = new Div(example.build());
+      div.setWidth("50%");
+      add(div);
    }
 }
---
+++ src/main/java/net/steinerworld/hypnobook/ui/views/dashboard/PieChartExample.java
@@ -0,0 +1,29 @@
+package net.steinerworld.hypnobook.ui.views.dashboard;
+
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.ChartBuilder;
+import com.github.appreciated.apexcharts.config.builder.LegendBuilder;
+import com.github.appreciated.apexcharts.config.builder.ResponsiveBuilder;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.legend.Position;
+import com.github.appreciated.apexcharts.config.responsive.builder.OptionsBuilder;
+
+public class PieChartExample extends ApexChartsBuilder {
+
+   public PieChartExample() {
+      withChart(ChartBuilder.get().withType(Type.PIE).build())
+            .withLabels("Team A", "Team B", "Team C", "Team D", "Team E")
+            .withLegend(LegendBuilder.get()
+                  .withPosition(Position.RIGHT)
+                  .build())
+            .withSeries(44.0, 55.0, 13.0, 43.0, 22.0)
+            .withResponsive(ResponsiveBuilder.get()
+                  .withBreakpoint(480.0)
+                  .withOptions(OptionsBuilder.get()
+                        .withLegend(LegendBuilder.get()
+                              .withPosition(Position.BOTTOM)
+                              .build())
+                        .build())
+                  .build());
+   }
+}
