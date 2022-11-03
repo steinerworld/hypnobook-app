59e7ad9326c287842d9ca53e20d07b2aff0a1916
Steiner Michael <michael.steiner@adcubum.com>
2022-11-03 15:32:03 +0100

    immer noch Manifest

--- src/main/java/net/steinerworld/hypnobook/ui/views/about/AboutView.java
+++ src/main/java/net/steinerworld/hypnobook/ui/views/about/AboutView.java
@@ -1,5 +1,10 @@
 package net.steinerworld.hypnobook.ui.views.about;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.jar.Manifest;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.security.PermitAll;
 
@@ -11,6 +16,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import net.steinerworld.hypnobook.exceptions.MaloneyException;
 import net.steinerworld.hypnobook.ui.views.MainLayout;
 
 
@@ -22,20 +28,34 @@ public class AboutView extends VerticalLayout {
     public void initialize() {
         setSpacing(false);
 
-        String row = AboutView.class.getPackage().getImplementationVersion();
-        String version = row == null ? "" : row;
-        add(new H2("Hypno-Book " + version));
+        add(new H2("Hypno-Book"));
+        add(new Paragraph(getFormattedBuildInfo()));
+
         Image img = new Image("images/AlterEgoFull.jpeg", "Alter Ego");
         img.setWidth("200px");
         add(img);
 
-        add(new H4("Developed with love by steinerworld in 2022"));
-        add(new Paragraph("I wish you a successful future for your company Hypnose Steiner. \uD83D\uDC98"));
+        add(new H4("Developed with pleasure by steinerworld in 2022"));
+        add(new Paragraph("for my beloved wife and her business 'Hypnose Steiner'. \uD83D\uDC98"));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+    }
+
+    private String getFormattedBuildInfo() {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+        try {
+            Manifest manifest = new Manifest(stream);
+            String rowBuildDate = manifest.getMainAttributes().getValue("Build-date");
+            String rowVersion = AboutView.class.getPackage().getImplementationVersion();
+            String version = Objects.requireNonNullElse(rowVersion, "for development");
+            String build = Objects.requireNonNullElse(rowBuildDate, "now build");
+            return String.format("Version %s - %s", version, build);
+        } catch (IOException e) {
+            throw new MaloneyException("cannot read the manifest", e);
+        }
     }
 
 }
