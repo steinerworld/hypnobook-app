ffede7a2ff78eeae339dfeb8e5a6277737d6229e
Steiner Michael <michael.steiner@adcubum.com>
2022-11-29 17:24:08 +0100

    Kinderkrankheiten, was sonst?

--- src/main/java/net/steinerworld/hypnobook/pdf/HeaderFooterPageEvent.java
+++ src/main/java/net/steinerworld/hypnobook/pdf/HeaderFooterPageEvent.java
@@ -1,11 +1,14 @@
 package net.steinerworld.hypnobook.pdf;
 
+import java.io.IOException;
+
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
-import com.itextpdf.text.ExceptionConverter;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.ColumnText;
@@ -14,12 +17,9 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import net.steinerworld.hypnobook.exceptions.MaloneyException;
 import net.steinerworld.hypnobook.services.JournalSheetService;
 
-/*
-https://memorynotfound.com/adding-header-footer-pdf-using-itext-java/
-https://www.javamadesoeasy.com/2016/06/how-to-set-header-and-footer-in-pdf-in.html
- */
 public class HeaderFooterPageEvent extends PdfPageEventHelper {
 
    public void onStartPage(PdfWriter writer, Document document) {
@@ -28,39 +28,43 @@ public class HeaderFooterPageEvent extends PdfPageEventHelper {
       PdfPTable header = new PdfPTable(2);
       try {
          // set defaults
-         header.setWidths(new int[] {2, 24});
+         header.setWidths(new int[] {1, 3});
          header.setTotalWidth(rect.getWidth());
          header.getDefaultCell().setFixedHeight(40);
          header.getDefaultCell().setBorder(Rectangle.BOTTOM);
          header.getDefaultCell().setBorderColor(BaseColor.LIGHT_GRAY);
 
          // add image
-         PdfPCell logo = new PdfPCell();
-         logo.setPaddingBottom(15);
-         logo.setPaddingLeft(10);
-         logo.setBorder(Rectangle.BOTTOM);
-         logo.setBorderColor(BaseColor.LIGHT_GRAY);
-         logo.addElement(new Phrase("LOGO :-)", new Font(Font.FontFamily.HELVETICA, 12)));
-         header.addCell(logo);
-
-         //         Image logo = Image.getInstance(HeaderFooterPageEvent.class.getResource("/icons/icon.png"));
-         //         header.addCell(logo);
-
+         Image logo = Image.getInstance(HeaderFooterPageEvent.class.getResource("/serversideimages/LogoHypno.jpg"));
+         PdfPCell kopf = new PdfPCell();
+         kopf.setPaddingBottom(15);
+         kopf.setPaddingLeft(25);
+         kopf.setBorder(Rectangle.BOTTOM);
+         kopf.setBorderColor(BaseColor.LIGHT_GRAY);
+         kopf.addElement(logo);
+         header.addCell(kopf);
 
          // add text
          PdfPCell text = new PdfPCell();
+         text.setPaddingTop(10);
          text.setPaddingBottom(15);
-         text.setPaddingLeft(10);
+         text.setPaddingRight(25);
+
          text.setBorder(Rectangle.BOTTOM);
          text.setBorderColor(BaseColor.LIGHT_GRAY);
-         text.addElement(new Phrase("Buchungsjournal", new Font(Font.FontFamily.HELVETICA, 12)));
-         text.addElement(new Phrase("Hypnose Steiner, Fabrikstrasse 6, 4556 Biberist", new Font(Font.FontFamily.HELVETICA, 8)));
+         Paragraph p1 = new Paragraph("Hypnose Steiner", new Font(Font.FontFamily.HELVETICA, 10));
+         p1.setAlignment(Element.ALIGN_RIGHT);
+         text.addElement(p1);
+
+         Paragraph p2 = new Paragraph("Fabrikstrasse 6, 4556 Biberist", new Font(Font.FontFamily.HELVETICA, 8));
+         p2.setAlignment(Element.ALIGN_RIGHT);
+         text.addElement(p2);
          header.addCell(text);
 
          // write content
          header.writeSelectedRows(0, -1, rect.getLeft(), rect.getTop() - 10, writer.getDirectContent());
-      } catch (DocumentException e) {
-         throw new ExceptionConverter(e);
+      } catch (DocumentException | IOException e) {
+         throw new MaloneyException("cannot build Header or Footer section", e);
       }
    }
 
